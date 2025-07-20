2bd7c0ce41038c9be87c7aa677854ab2c0848603
Ajay Singh <90156893+Ajay080@users.noreply.github.com>
2025-07-20 09:18:53 +0530

    removed all warnning of deployent

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config(); //Loads environent file variables
+dotenv.config();
+
+mongoose.set('strictQuery', false); // Add this line
 
 const connectDB=async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, // use new url string parser,
-            useUnifiedTopology: true, // use new server discovery and monitoring engine
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         })
     }
     catch(e){
         console.error("Error connecting to the MongoDB", e);
-        process.exit(1); // Exit the process with failure
+        process.exit(1);
     }
 }
 
